ec57bd13b988d5c77fc5b3ded765430bd6e08c22
Simon Southwell <simon@anita-simulators.org.uk>
2024-10-06 11:49:59 +0100

    Added copyright/licence banners

--- bfm/test/testav.v
+++ bfm/test/testav.v
@@ -1,3 +1,25 @@
+// =============================================================
+//
+// Top level test environment for Verilog Avalon BFM VProc wrapper
+//
+// Copyright (c) 2024 Simon Southwell. Confidential
+//
+// This file is part of VProc.
+//
+// VProc is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// VProc is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with VProc. If not, see <http://www.gnu.org/licenses/>.
+//
+// =============================================================
 
 `timescale 1 ns / 10 ps
 
